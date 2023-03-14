3d5f5a140b76d61e1ca42105b964952faa68695a
Natalia Blanco <nataliaouteiral@gmail.com>
2023-03-14 11:36:09 +0100

    add some props

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,8 @@ import '../styles/Dummy.scss';
 import '../styles/Letters.scss';
 import '../styles/Form.scss';
 import '../styles/Header.scss';
+import Header from './Header';
+import Errors from './Dummy';
 
 function App() {
   const [word, setWord] = useState('');
@@ -28,7 +30,7 @@ function App() {
   };
 
   const handleChange = (ev) => {
-    let re = /^[a-zA-ZñÑá-úÁ-Ú´]$/; //add regular pattern 
+    let re = /^[a-zA-ZñÑá-úÁ-Ú´]$/; //add regular pattern
     if (re.test(ev.target.value) || ev.target.value === '') {
       handleLastLetter(ev.target.value);
     }
@@ -50,7 +52,7 @@ function App() {
     return wordLetters.map((letter, index) => {
       const exists = userLetters.includes(letter.toLocaleLowerCase());
       return (
-        <li key={index} className='letter'>
+        <li key={index} className="letter">
           {exists ? letter : ''}
         </li>
       );
@@ -64,7 +66,7 @@ function App() {
     );
     return errorLetters.map((letter, index) => {
       return (
-        <li key={index} className='letter'>
+        <li key={index} className="letter">
           {letter}
         </li>
       );
@@ -82,53 +84,37 @@ function App() {
   };
 
   return (
-    <div className='page'>
-      <header>
-        <h1 className='header__title'>Juego del ahorcado</h1>
-      </header>
-      <main className='main'>
+    <div className="page">
+      <Header />
+      <main className="main">
         <section>
-          <div className='solution'>
-            <h2 className='title'>Solución:</h2>
-            <ul className='letters'>{renderSolutionLetters()}</ul>
+          <div className="solution">
+            <h2 className="title">Solución:</h2>
+            <ul className="letters">{renderSolutionLetters()}</ul>
           </div>
-          <div className='error'>
-            <h2 className='title'>Letras falladas:</h2>
-            <ul className='letters'>{renderErrorLetters()}</ul>
+          <div className="error">
+            <h2 className="title">Letras falladas:</h2>
+            <ul className="letters">{renderErrorLetters()}</ul>
           </div>
-          <form className='form' onSubmit={handleSubmit}>
-            <label className='title' htmlFor='last-letter'>
+          <form className="form" onSubmit={handleSubmit}>
+            <label className="title" htmlFor="last-letter">
               Escribe una letra:
             </label>
             <input
               autoFocus
-              autoComplete='off'
-              className='form__input'
-              maxLength='1'
-              type='text'
-              name='last-letter'
-              id='last-letter'
+              autoComplete="off"
+              className="form__input"
+              maxLength="1"
+              type="text"
+              name="last-letter"
+              id="last-letter"
               value={lastLetter}
               onKeyDown={handleKeyDown}
               onChange={handleChange}
             />
           </form>
         </section>
-        <section className={`dummy error-${getNumberOfErrors()}`}>
-          <span className='error-13 eye'></span>
-          <span className='error-12 eye'></span>
-          <span className='error-11 line'></span>
-          <span className='error-10 line'></span>
-          <span className='error-9  line'></span>
-          <span className='error-8  line'></span>
-          <span className='error-7  line'></span>
-          <span className='error-6  head'></span>
-          <span className='error-5  line'></span>
-          <span className='error-4  line'></span>
-          <span className='error-3  line'></span>
-          <span className='error-2  line'></span>
-          <span className='error-1  line'></span>
-        </section>
+        <Errors numberOfErrors="" />
       </main>
     </div>
   );
